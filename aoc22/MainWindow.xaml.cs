4111c82bb350d99ea61d9934fd00d7aa895f5192
CrushaKRool <CrushaKRool@users.noreply.github.com>
2022-12-01 22:51:48 +0100

    Initial project structure and solver for the puzzles of the first day. Will probably refactor some things as I see how the puzzles change throughout the month.

---
+++ aoc22/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using aoc22.Puzzles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace aoc22
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void btnSolve_Click(object sender, RoutedEventArgs e)
+    {
+      // TODO: Make the puzzle selectable.
+      IPuzzleSolver solver = new Day1();
+      try
+      {
+        SetOutput(solver.Compute(GetInput()));
+      }
+      catch (Exception ex)
+      {
+        SetOutput($"ERROR: {ex.Message}");
+      }
+    }
+
+    /// <summary>
+    /// Gets the input text for the puzzle.
+    /// </summary>
+    /// <returns>The input for the puzzle.</returns>
+    private string GetInput()
+    {
+      return tbInput.Text;
+    }
+
+    /// <summary>
+    /// Shows the computed output of the puzzle in the UI.
+    /// </summary>
+    /// <param name="output">Output to show.</param>
+    private void SetOutput(string output)
+    {
+      tbOutput.Text = output;
+    }
+  }
+}
---
+++ aoc22/Puzzles/Day1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Documents;
+
+namespace aoc22.Puzzles
+{
+  class Day1 : IPuzzleSolver
+  {
+    public string PuzzleName => "Day 1: Calorie Counting";
+
+    public string Compute(string input)
+    {
+      return Part1(input) + Environment.NewLine + Part2(input);
+    }
+
+    /// <summary>
+    /// How many calories is the elf with the most calories carrying?
+    /// </summary>
+    /// <param name="input">One numeric calorie per line, or a blank line to indicate the end of the current elf's load.</param>
+    private static string Part1(string input)
+    {
+      List<long> calorieSums = GetCalorieSums(input);
+
+      // The puzzle actually only asks for the calorie amount, but let's output the index of the elf as well.
+      long maxCalories = calorieSums.Max();
+      long elfIndex = calorieSums.IndexOf(maxCalories);
+      return $"The Elf carrying the most is the {elfIndex + 1}th Elf with {maxCalories} Calories.";
+    }
+
+    /// <summary>
+    /// How many total calories are the three elves with the most calories carrying?
+    /// </summary>
+    /// <param name="input">One numeric calorie per line, or a blank line to indicate the end of the current elf's load.</param>
+    private static string Part2(string input)
+    {
+      List<long> calorieSums = GetCalorieSums(input);
+
+      // By sorting, we lose track of the original index numbers in this data structure. But since those are not required in the answer...
+      calorieSums.Sort();
+      calorieSums.Reverse();
+      long topThreeTotal = calorieSums.Take(3).Sum();
+      return $"The top 3 Elves are carrying a total of {topThreeTotal} Calories.";
+    }
+
+    private static List<long> GetCalorieSums(string input)
+    {
+      List<long> calorieSums = new();
+      long curCalories = 0;
+
+      foreach (string line in input.Split(Environment.NewLine, StringSplitOptions.TrimEntries))
+      {
+        if (string.IsNullOrEmpty(line))
+        {
+          calorieSums.Add(curCalories);
+          curCalories = 0;
+        }
+        else
+        {
+          if (long.TryParse(line, out long parsedCalories))
+          {
+            curCalories += parsedCalories;
+          }
+          else
+          {
+            throw new ArgumentException("Invalid line in input:" + Environment.NewLine + line);
+          }
+        }
+      }
+
+      return calorieSums;
+    }
+  }
+}

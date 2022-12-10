c54ff9305fbdd1c91a5ca89700a85d2d530fcd9a
CrushaKRool <CrushaKRool@users.noreply.github.com>
2022-12-10 13:30:34 +0100

    Solved day 10.

--- aoc22/MainWindow.xaml.cs
+++ aoc22/MainWindow.xaml.cs
@@ -29,7 +29,7 @@ namespace aoc22
     private void btnSolve_Click(object sender, RoutedEventArgs e)
     {
       // TODO: Make the puzzle selectable.
-      IPuzzleSolver solver = new Day9();
+      IPuzzleSolver solver = new Day10();
       try
       {
         SetOutput1(solver.SolvePart1(GetInput()));
---
+++ aoc22/Puzzles/Day10.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+
+namespace aoc22.Puzzles
+{
+  class Day10 : IPuzzleSolver
+  {
+    public string PuzzleName => "Day 10: Cathode-Ray Tube";
+
+    public string SolvePart1(string input)
+    {
+      Queue<string> instructionQueue = new(input.Split(Environment.NewLine).Where(l => !string.IsNullOrWhiteSpace(l)));
+      SimpleCpu cpu = new(instructionQueue);
+      int signalStrengthSum = 0;
+      while (!cpu.FinishedAllInstructions)
+      {
+        cpu.Tick();
+        if ((cpu.Cycle - 20) % 40 == 0)
+        {
+          signalStrengthSum += cpu.Cycle * cpu.X;
+        }
+      }
+
+      return $"The sum of signal strengths is {signalStrengthSum}.";
+    }
+
+    public string SolvePart2(string input)
+    {
+      StringBuilder sb = new();
+
+      Queue<string> instructionQueue = new(input.Split(Environment.NewLine).Where(l => !string.IsNullOrWhiteSpace(l)));
+      SimpleCpu cpu = new(instructionQueue);
+      while (!cpu.FinishedAllInstructions)
+      {
+        if (Math.Abs(cpu.X - ((cpu.Cycle-1) % 40)) < 2)
+        {
+          sb.Append('#');
+        }
+        else
+        {
+          sb.Append('.');
+        }
+        if ((cpu.Cycle) % 40 == 0)
+        {
+          sb.AppendLine();
+        }
+        cpu.Tick();
+      }
+
+      return sb.ToString();
+    }
+
+    class SimpleCpu
+    {
+      public int Cycle { get; set; } = 1;
+      public int X { get; set; } = 1;
+
+      public bool FinishedAllInstructions { get => InstructionQueue.Count == 0; }
+
+      private readonly Queue<string> InstructionQueue;
+
+      private string? CurInstruction;
+      private int CurInstructionCycleStart;
+
+      public SimpleCpu(Queue<string> instructionQueue)
+      {
+        InstructionQueue = instructionQueue;
+      }
+
+      public void Tick()
+      {
+        if (string.IsNullOrEmpty(CurInstruction))
+        {
+          if (FinishedAllInstructions)
+          {
+            return;
+          }
+          CurInstruction = InstructionQueue.Dequeue();
+          CurInstructionCycleStart = Cycle;
+        }
+        bool finishedProcessing = ProcessInstruction(CurInstruction);
+        if (finishedProcessing)
+        {
+          CurInstruction = null;
+        }
+        Cycle++;
+      }
+
+      private bool ProcessInstruction(string instruction)
+      {
+        string[] instructionParts = instruction.Split(" ");
+        switch (instructionParts[0])
+        {
+          case "noop":
+            return true;
+          case "addx":
+            if (Cycle - CurInstructionCycleStart >= 1)
+            {
+              X += int.Parse(instructionParts[1]);
+              return true;
+            }
+            return false;
+          default:
+            throw new ArgumentException($"Unknown instruction: {instruction}");
+        }
+      }
+    }
+  }
+}

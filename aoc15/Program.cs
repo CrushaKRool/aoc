17d7cd093348423200fd0a278cf05471f52b0e9d
CrushaKRool <CrushaKRool@users.noreply.github.com>
2022-12-11 18:35:35 +0100

    Started solving AoC 15, using a console application approach and integrated test data as a test for future refactorings in AoC 22.

---
+++ aoc15/Program.cs
@@ -0,0 +1,51 @@
+﻿using aoc15.Puzzles;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc15
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Enter day number to run: ");
+      string? line = Console.ReadLine();
+      if (int.TryParse(line, out int dayNumber))
+      {
+        IPuzzle dayPuzzle;
+        try
+        {
+          string fqn = $"aoc15.Puzzles.Day{dayNumber:D2}.Day{dayNumber:D2}";
+          dayPuzzle = (IPuzzle)Activator.CreateInstance(null, fqn).Unwrap();
+        }
+        catch (Exception ex)
+        {
+          Console.WriteLine($"Unable to instantiate class 'Day{dayNumber:D2}'. {ex.Message}");
+          return;
+        }
+        if (dayPuzzle == null)
+        {
+          Console.WriteLine($"Unable to instantiate class 'Day{dayNumber}'.");
+          return;
+        }
+        RunPuzzle(dayPuzzle);
+      }
+      else
+      {
+        Console.WriteLine("Input must be a number.");
+      }
+    }
+
+    private static void RunPuzzle(IPuzzle puzzle)
+    {
+      string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), puzzle.GetInputFileName());
+      string input = File.ReadAllText(path);
+      puzzle.Run(input);
+    }
+  }
+}
---
+++ aoc15/Puzzles/Day01/Day01.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc15.Puzzles.Day01
+{
+  public class Day01 : IPuzzle
+  {
+    public string GetInputFileName()
+    {
+      return @"Puzzles\Day01\Day01Input.txt";
+    }
+
+    public void Run(string input)
+    {
+      int floor = 0;
+      int curPos = 0;
+      bool enteredBasement = false;
+      foreach (char c in input)
+      {
+        curPos++;
+        switch(c)
+        {
+          case '(':
+            floor++;
+            break;
+          case ')':
+            floor--;
+            break;
+        }
+        if (floor < 0 && !enteredBasement)
+        {
+          Console.WriteLine($"Entering basement at position {curPos}.");
+          enteredBasement = true;
+        }
+      }
+      Console.WriteLine($"The instructions will take us to floor {floor}.");
+    }
+  }
+}

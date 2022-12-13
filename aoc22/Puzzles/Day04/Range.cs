103e8ea828e4f2b56bd8b701ee4dc7ad63cf345f
CrushaKRool <CrushaKRool@users.noreply.github.com>
2022-12-13 21:53:48 +0100

    Refactored day 4 to console app.

---
+++ aoc22/Puzzles/Day04/Range.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc22.Puzzles.Day04
+{
+  /// <summary>
+  /// Represents a range between a given minimum and maximum value.
+  /// </summary>
+  internal sealed class Range
+  {
+    public int Min { get; }
+    public int Max { get; }
+
+    public Range(int min, int max)
+    {
+      if (min <= max)
+      {
+        Min = min;
+        Max = max;
+      }
+      else
+      {
+        Min = max;
+        Max = min;
+      }
+    }
+
+    /// <summary>
+    /// Indicates whether this range completely contains the given other range.
+    /// </summary>
+    /// <param name="other">Other range to compare this one to.</param>
+    /// <returns>true, if this range fully contains the other.</returns>
+    public bool ContainsOther(Range other)
+    {
+      return Min <= other.Min && Max >= other.Max;
+    }
+
+    /// <summary>
+    /// Indicates whether this range overlaps the given other range.
+    /// </summary>
+    /// <param name="other">Other range to compare this one to.</param>
+    /// <returns>true, if this range overlaps the other.</returns>
+    public bool OverlapsOther(Range other)
+    {
+      return Min <= other.Min && Max >= other.Min || Min <= other.Max && Max >= other.Max;
+    }
+  }
+}

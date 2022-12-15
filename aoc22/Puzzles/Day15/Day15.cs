fcce313fc7b542742ba6ff7cc6f874681b357b2a
CrushaKRool <CrushaKRool@users.noreply.github.com>
2022-12-15 22:39:17 +0100

    Solved day 15. This took me way too long.

---
+++ aoc22/Puzzles/Day15/Day15.cs
@@ -0,0 +1,126 @@
+﻿using aoc_common;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace aoc22.Puzzles.Day15
+{
+  public class Day15 : IPuzzle
+  {
+    private const int CoordinateMin = 0;
+    private const int CoordinateMax = 4000000;
+
+    private readonly Regex LineParser = new("Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)");
+
+    public string PuzzleName => "Day 15: Beacon Exclusion Zone";
+
+    public string InputFileName => @"Puzzles\Day15\Day15Input.txt";
+
+    public void Run(string input)
+    {
+      List<Sensor> sensors = new();
+      foreach (string inputLine in input.Split(Environment.NewLine).Where(l => !string.IsNullOrEmpty(l)))
+      {
+        Match m = LineParser.Match(inputLine);
+        if (!m.Success)
+        {
+          throw new ArgumentException($"Unable to parse line {inputLine}.");
+        }
+        sensors.Add(new(
+          int.Parse(m.Groups[1].Value),
+          int.Parse(m.Groups[2].Value),
+          int.Parse(m.Groups[3].Value),
+          int.Parse(m.Groups[4].Value)
+          ));
+      }
+
+      const int referenceY = 2000000;
+      ISet<int> coveredPositions = new HashSet<int>();
+      foreach (Sensor sensor in sensors)
+      {
+        foreach (int coveredX in sensor.GetCoveredXCoordinatesForY(referenceY))
+        {
+          coveredPositions.Add(coveredX);
+        }
+      }
+
+      Console.WriteLine($"{coveredPositions.Count} can not contain a beacon.");
+
+      // Part 2
+
+      // To cut down on the number of coordinates we have to check, calculate the perimeter of each sensor.
+      // Any points inside a sensor's range are no candidates, so only consider the points exactly outside of the edge bounds.
+      // For each of them, check if they are within the range of other sensors.
+      // Some perimeter points from different sensors may be overlapping.
+      Point point = sensors
+        .AsParallel()
+        .SelectMany(sensor => sensor.GetAllPerimeterPoints())
+        .Where(ep => PointWithinBounds(ep) && !PointInAnySensorRange(ep, sensors))
+        .Distinct()
+        .Single();
+
+      BigInteger frequency = new BigInteger(point.X) * new BigInteger(4000000) + new BigInteger(point.Y);
+      Console.WriteLine($"Found candidate at ({point.X},{point.Y}) with a frequency of {frequency}.");
+    }
+
+    private static bool PointInAnySensorRange(Point point, List<Sensor> sensors)
+    {
+      return sensors.Any(s => s.PointInRange(point.X, point.Y));
+    }
+
+    private static bool PointWithinBounds(Point point)
+    {
+      return point.X >= CoordinateMin && point.X <= CoordinateMax && point.Y >= CoordinateMin && point.Y <= CoordinateMax;
+    }
+
+    /// <summary>
+    /// Visualization to debug the test input. Won't work with the actual input file due to its size.
+    /// </summary>
+    /// <param name="sensors"></param>
+    private static void VisualizeEdgePoints(List<Sensor> sensors)
+    {
+      char[,] visualization = new char[50, 50];
+      for (int x = 0; x < 50; x++)
+      {
+        for (int y = 0; y < 50; y++)
+        {
+          visualization[x, y] = '.';
+        }
+      }
+
+      foreach (Sensor sensor in sensors)
+      {
+        foreach (Point ep in sensor.GetAllPerimeterPoints())
+        {
+          if (PointWithinBounds(ep))
+          {
+            visualization[ep.X, ep.Y] = 'o';
+          }
+        }
+      }
+      foreach (Sensor sensor in sensors)
+      {
+        if (PointWithinBounds(new Point(sensor.X, sensor.Y)))
+        {
+          visualization[sensor.X, sensor.Y] = 'S';
+        }
+        if (PointWithinBounds(new Point(sensor.ClosestBeaconX, sensor.ClosestBeaconY)))
+        {
+          visualization[sensor.ClosestBeaconX, sensor.ClosestBeaconY] = 'B';
+        }
+      }
+
+      for (int y = 0; y < 50; y++)
+      {
+        for (int x = 0; x < 50; x++)
+        {
+          Console.Write(visualization[x, y]);
+        }
+        Console.WriteLine();
+      }
+    }
+  }
+}
---
+++ aoc22/Puzzles/Day15/Sensor.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace aoc22.Puzzles.Day15
+{
+  internal class Sensor
+  {
+    public int X { get; }
+    public int Y { get; }
+
+    public int ClosestBeaconX { get; }
+    public int ClosestBeaconY { get; }
+
+    public int ManhattenDistance { get; }
+
+    public Sensor(int x, int y, int closestBeaconX, int closestBeaconY)
+    {
+      X = x;
+      Y = y;
+      ClosestBeaconX = closestBeaconX;
+      ClosestBeaconY = closestBeaconY;
+      ManhattenDistance = Math.Abs(x - ClosestBeaconX) + Math.Abs(y - ClosestBeaconY);
+    }
+
+    /// <summary>
+    /// Returns the number of positions that are covered by this sensor in the given Y row.
+    /// </summary>
+    /// <param name="referenceY">Y coordinate of the row to check.</param>
+    /// <returns>Number of covered positions.</returns>
+    public int GetBlockedXRangeForY(int referenceY)
+    {
+      return Math.Max(ManhattenDistance - Math.Abs(Y - referenceY), 0);
+    }
+
+    /// <summary>
+    /// Returns the number of positions that are covered by this sensor in the given X column.
+    /// </summary>
+    /// <param name="referenceX">X coordinate of the column to check.</param>
+    /// <returns>Number of covered positions.</returns>
+    public int GetBlockedYRangeForX(int referenceX)
+    {
+      return Math.Max(ManhattenDistance - Math.Abs(X - referenceX), 0);
+    }
+
+    /// <summary>
+    /// Returns an enumerable of all X coordinates that are within range of this sensor in the line with the given Y coordinate, based on this sensor's Manhatten distance.
+    /// </summary>
+    /// <param name="referenceY">Y coordinate of the line for which to find all covered X coordinates.</param>
+    /// <returns>List of all X coordinates covered by this sensor in row Y.</returns>
+    public List<int> GetCoveredXCoordinatesForY(int referenceY)
+    {
+      List<int> result = new();
+
+      int remainingDistance = GetBlockedXRangeForY(referenceY);
+      if (remainingDistance <= 0)
+      {
+        return result;
+      }
+
+      for (int x = X - remainingDistance; x < X + remainingDistance; x++)
+      {
+        result.Add(x);
+      }
+      return result;
+    }
+
+    /// <summary>
+    /// Returns an enumerable of all Y coordinates that are within range of this sensor in the line with the given X coordinate, based on this sensor's Manhatten distance.
+    /// </summary>
+    /// <param name="referenceY">X coordinate of the line for which to find all covered Y coordinates.</param>
+    /// <returns>List of all Y coordinates covered by this sensor in row X.</returns>
+    public List<int> GetCoveredYCoordinatesForX(int referenceX)
+    {
+      List<int> result = new();
+
+      int remainingDistance = GetBlockedYRangeForX(referenceX);
+      if (remainingDistance <= 0)
+      {
+        return result;
+      }
+
+      for (int y = Y - remainingDistance; y < Y + remainingDistance; y++)
+      {
+        result.Add(y);
+      }
+      return result;
+    }
+
+    /// <summary>
+    /// Gets all coordinates that are exactly outside of the Manhatten distance described by this sensor.
+    /// </summary>
+    /// <returns>List of all points on the edge.</returns>
+    public IEnumerable<Point> GetAllPerimeterPoints()
+    {
+      for (int pointY = Y - ManhattenDistance - 1; pointY <= Y + ManhattenDistance + 1; pointY++)
+      {
+        int remainingDistance = Math.Max(ManhattenDistance + 1 - Math.Abs(Y - pointY), 0);
+        Point a = new(X - remainingDistance, pointY);
+        Point b = new(X + remainingDistance, pointY);
+        yield return a;
+        if (a != b)
+        {
+          yield return b;
+        }
+      }
+    }
+
+    public bool PointInRange(int checkX, int checkY)
+    {
+      int pointDistance = Math.Abs(X - checkX) + Math.Abs(Y - checkY);
+      return pointDistance <= ManhattenDistance;
+    }
+  }
+}

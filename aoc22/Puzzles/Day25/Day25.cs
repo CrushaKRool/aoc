3027ade2ccd87be6737f26a75658a62bed24f9b2
CrushaKRool <CrushaKRool@users.noreply.github.com>
2022-12-25 14:08:29 +0100

    Solved day 25.

---
+++ aoc22/Puzzles/Day25/Day25.cs
@@ -0,0 +1,27 @@
+﻿using aoc_common;
+using System;
+
+namespace aoc22.Puzzles.Day25
+{
+  public class Day25 : IPuzzle
+  {
+    public string PuzzleName => "Day 25: Full of Hot Air";
+
+    public string InputFileName => @"Puzzles\Day25\Day25Input.txt";
+
+    public void Run(string input)
+    {
+      long sum = 0;
+      foreach (string inputLine in input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+      {
+        long decNum = SnafuConverter.FromSnafu(inputLine);
+#if DEBUG
+        Console.WriteLine($"{inputLine, 30} : {decNum, 30} : {SnafuConverter.ToSnafu(decNum), 30}");
+#endif
+        sum += decNum;
+      }
+
+      Console.WriteLine($"The sum is {sum}, which is '{SnafuConverter.ToSnafu(sum)}' in SNAFU.");
+    }
+  }
+}
---
+++ aoc22/Puzzles/Day25/SnafuConverter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc22.Puzzles.Day25
+{
+  internal class SnafuConverter
+  {
+    public static long FromSnafu(string snafu)
+    {
+      int[] digits = new int[snafu.Length];
+
+      for (int i = 0; i < snafu.Length; i++)
+      {
+        char c = snafu[snafu.Length - 1 - i];
+        digits[i] = c switch
+        {
+          '=' => -2,
+          '-' => -1,
+          '0' => 0,
+          '1' => 1,
+          '2' => 2,
+          _ => throw new ArgumentException($"Unknown snafu digit: {c}")
+        };
+      }
+
+      long result = 0;
+      for (int i = 0; i < digits.Length; i++)
+      {
+        result += digits[i] * (long)Math.Pow(5, i);
+      }
+      return result;
+    }
+
+    public static string ToSnafu(long number)
+    {
+      StringBuilder sb = new();
+
+      while (number != 0)
+      {
+        long digitIndex = (number + 2) % 5;
+        sb.Insert(0, digitIndex switch
+        {
+          0 => '=',
+          1 => '-',
+          2 => '0',
+          3 => '1',
+          4 => '2',
+          _ => new ArgumentException($"Unexpected index: {digitIndex}"),
+        });
+        number = (number + 2) / 5;
+      }
+
+      return sb.ToString();
+    }
+  }
+}

f424c1b16530b5c92f8688e2d590f485397a090b
Crusha K. Rool <CrushaKRool@users.noreply.github.com>
2022-12-30 18:14:49 +0100

    Solved AoC 15 day 4.

---
+++ aoc15/Puzzles/Day04/Day04.cs
@@ -0,0 +1,42 @@
+﻿using aoc_common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc15.Puzzles.Day04
+{
+  public class Day04 : IPuzzle
+  {
+    public string PuzzleName => "Day 4: The Ideal Stocking Stuffer";
+
+    public string InputFileName => @"Input.txt";
+
+    public void Run(string input)
+    {
+      int i = 0;
+      string hash;
+      do
+      {
+        i++;
+        byte[] inputBytes = Encoding.ASCII.GetBytes(input + i);
+        byte[] hashBytes = MD5.HashData(inputBytes);
+        hash = Convert.ToHexString(hashBytes);
+      } while (!hash.StartsWith("00000"));
+
+      Console.WriteLine($"The positive number {i} results in the MD5 hash {hash}, which starts with five zeros.");
+
+      do
+      {
+        i++;
+        byte[] inputBytes = Encoding.ASCII.GetBytes(input + i);
+        byte[] hashBytes = MD5.HashData(inputBytes);
+        hash = Convert.ToHexString(hashBytes);
+      } while (!hash.StartsWith("000000"));
+
+      Console.WriteLine($"The positive number {i} results in the MD5 hash {hash}, which starts with six zeros.");
+    }
+  }
+}

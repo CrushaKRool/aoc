9532bbca98f81daaf33b72537897559b8d7a05ad
CrushaKRool <CrushaKRool@users.noreply.github.com>
2023-12-02 13:27:06 +0100

    Solved Day 02.

---
+++ aoc23/Puzzles/Day02/Day02.cs
@@ -0,0 +1,57 @@
+﻿using aoc_common;
+using System.Text.RegularExpressions;
+
+namespace aoc23.Puzzles.Day02
+{
+  public partial class Day02 : IPuzzle
+  {
+    public string PuzzleName => "Day 02: Cube Conundrum";
+
+    public string InputFileName => @"Input.txt";
+
+    public void Run(string input)
+    {
+      Console.WriteLine("Part 1:");
+      Part1(input);
+
+      Console.WriteLine("-----------------------------------------------------");
+
+      Console.WriteLine("Part 2:");
+      Part2(input);
+    }
+
+    private static void Part1(string input)
+    {
+      long sum = 0;
+      foreach (string line in input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+      {
+        GameData game = new(line);
+        if (IsGamePossible(game, 12, 13, 14))
+        {
+          Console.WriteLine($"Game {game.GameNumber} is possible.");
+          sum += game.GameNumber;
+        }
+      }
+      Console.WriteLine("Sum of Game IDs: " + sum);
+    }
+
+    private static bool IsGamePossible(GameData game, int availableRed, int availableGreen, int availableBlue)
+    {
+      return game.MaxRed <= availableRed
+        && game.MaxGreen <= availableGreen
+        && game.MaxBlue <= availableBlue;
+    }
+
+    private static void Part2(string input)
+    {
+      long sum = 0;
+      foreach (string line in input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+      {
+        GameData game = new(line);
+        Console.WriteLine($"Power of minimum set of cubes for game {game.GameNumber} is {game.PowerOfMinimumSet}.");
+        sum += game.PowerOfMinimumSet;
+      }
+      Console.WriteLine("Sum of Powers: " + sum);
+    }
+  }
+}
---
+++ aoc23/Puzzles/Day02/GameData.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.RegularExpressions;
+
+namespace aoc23.Puzzles.Day02
+{
+  internal partial class GameData
+  {
+    public string InputLine { get; }
+    public int GameNumber { get; }
+    public int MaxRed { get; }
+    public int MaxGreen { get; }
+    public int MaxBlue { get; }
+    public int PowerOfMinimumSet { get; }
+
+    private static readonly Regex Parser = GameParser();
+
+    public GameData(string line)
+    {
+      InputLine = line;
+      Match match = Parser.Match(line);
+      if (!match.Success)
+      {
+        throw new ArgumentException("Unexpected input format: " + line, nameof(line));
+      }
+      GameNumber = int.Parse(match.Groups[1].Value);
+      string[] cubeSets = match.Groups[2].Value.Split(';', StringSplitOptions.TrimEntries);
+      foreach (string cs in cubeSets)
+      {
+        CubeSet cubeSet = ParseCubeSet(cs);
+        MaxRed = Math.Max(MaxRed, cubeSet.Red);
+        MaxGreen = Math.Max(MaxGreen, cubeSet.Green);
+        MaxBlue = Math.Max(MaxBlue, cubeSet.Blue);
+      }
+      PowerOfMinimumSet = MaxRed * MaxGreen * MaxBlue;
+    }
+
+    private CubeSet ParseCubeSet(string part)
+    {
+      CubeSet result = new();
+      string[] cubes = part.Split(',', StringSplitOptions.TrimEntries);
+      foreach (string pair in cubes)
+      {
+        string[] countAndColor = pair.Split(' ', StringSplitOptions.TrimEntries);
+        int count = int.Parse(countAndColor[0]);
+        string color = countAndColor[1];
+        switch (color)
+        {
+          case "red":
+            result.Red = count;
+            break;
+          case "green":
+            result.Green = count;
+            break;
+          case "blue":
+            result.Blue = count;
+            break;
+        }
+      }
+      return result;
+    }
+
+    private struct CubeSet
+    {
+      public int Red { get; set; }
+      public int Green { get; set; }
+      public int Blue { get; set; }
+    }
+
+    [GeneratedRegex(@"^Game (\d+): (.+)$")]
+    private static partial Regex GameParser();
+  }
+}

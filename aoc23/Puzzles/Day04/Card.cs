77fc1bc4d3462c7fa37f678f660f467f2fa874a5
CrushaKRool <CrushaKRool@users.noreply.github.com>
2023-12-05 00:52:34 +0100

    Solved Day 04.

---
+++ aoc23/Puzzles/Day04/Card.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Design;
+
+namespace aoc23.Puzzles.Day04
+{
+  internal class Card
+  {
+    public int ID { get; }
+    public ISet<int> WinningNumbers { get; }
+    public int[] OwnNumbers { get; }
+    public int OwnWinningNumberCount { get; }
+
+    public int Count { get; set; } = 1;
+
+    public Card(string input)
+    {
+      string[] inputParts = input.Split(new char[] { ':', '|' }, StringSplitOptions.TrimEntries & StringSplitOptions.RemoveEmptyEntries);
+      ID = int.Parse(inputParts[0].Split(' ', StringSplitOptions.RemoveEmptyEntries)[1]);
+      WinningNumbers = inputParts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToHashSet();
+      OwnNumbers = inputParts[2].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+      OwnWinningNumberCount = OwnNumbers.Count(n => WinningNumbers.Contains(n));
+    }
+
+    public int GetPoints()
+    {
+      return OwnWinningNumberCount switch
+      {
+        0 => 0,
+        1 => 1,
+        _ => (int) Math.Pow(2, OwnWinningNumberCount - 1),
+      };
+    }
+  }
+}
--- aoc23/Puzzles/Day04/Day04.cs
+++ aoc23/Puzzles/Day04/Day04.cs
@@ -4,14 +4,54 @@ namespace aoc23.Puzzles.Day04
 {
   public class Day04 : IPuzzle
   {
-    public string PuzzleName => throw new NotImplementedException();
+    public string PuzzleName => "Day 4: Scratchcards";
 
-    public string InputFileName => @"Test.txt";
+    public string InputFileName => @"Input.txt";
 
     public void Run(string input)
     {
+      List<Card> cards = [];
+      foreach (string inputLine in input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+      {
+        cards.Add(new Card(inputLine));
+      }
+
+      Part1(cards);
+
+      Console.WriteLine("-----------------------------------------------------");
+
+      Part2(cards);
+    }
+
+    private static void Part1(List<Card> cards)
+    {
       Console.WriteLine("Part 1:");
-      throw new NotImplementedException();
+      int sum = 0;
+      foreach (Card card in cards)
+      {
+        int points = card.GetPoints();
+        Console.WriteLine($"Card {card.ID} is worth {points}.");
+        sum += points;
+      }
+      Console.WriteLine($"Sum of card points is {sum}.");
+    }
+
+    private static void Part2(List<Card> cards)
+    {
+      Console.WriteLine("Part 2:");
+      for (int i = 0; i < cards.Count; i++)
+      {
+        Card curCard = cards[i];
+        Console.WriteLine($"Card {curCard.ID} has {curCard.Count} instances.");
+        int winCount = curCard.OwnWinningNumberCount;
+        for (int j = i + 1; j < i + 1 + winCount && j < cards.Count; j++)
+        {
+          cards[j].Count += curCard.Count;
+        }
+      }
+
+      long cardCount = cards.Sum(c => c.Count);
+      Console.WriteLine($"Card count: {cardCount}");
     }
   }
 }

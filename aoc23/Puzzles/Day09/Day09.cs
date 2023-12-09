bd68fd777834e5a8a9c94074276949019814a815
CrushaKRool <CrushaKRool@users.noreply.github.com>
2023-12-09 13:47:19 +0100

    Solved Day 09.

--- aoc23/Puzzles/Day09/Day09.cs
+++ aoc23/Puzzles/Day09/Day09.cs
@@ -4,14 +4,46 @@ namespace aoc23.Puzzles.Day09
 {
   public class Day09 : IPuzzle
   {
-    public string PuzzleName => throw new NotImplementedException();
+    public string PuzzleName => "Day 9: Mirage Maintenance";
 
-    public string InputFileName => @"Test.txt";
+    public string InputFileName => @"Input.txt";
 
     public void Run(string input)
     {
+      List<History> histories = [];
+      foreach (string line in input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+      {
+        histories.Add(new(line));
+      }
+
+      Part1(histories);
+      Part2(histories);
+    }
+
+    private static void Part1(List<History> histories)
+    {
       Console.WriteLine("Part 1:");
-      throw new NotImplementedException();
+      int sum = 0;
+      foreach (var hist in histories)
+      {
+        int prediction = hist.PredictNextValue();
+        Console.WriteLine(prediction);
+        sum += prediction;
+      }
+      Console.WriteLine($"Sum of predicted next values: {sum}");
+    }
+
+    private static void Part2(List<History> histories)
+    {
+      Console.WriteLine("Part 2:");
+      int sum = 0;
+      foreach (var hist in histories)
+      {
+        int prediction = hist.PredictPreviousValue();
+        Console.WriteLine(prediction);
+        sum += prediction;
+      }
+      Console.WriteLine($"Sum of predicted previous values: {sum}");
     }
   }
 }
---
+++ aoc23/Puzzles/Day09/History.cs
@@ -0,0 +1,64 @@
+﻿namespace aoc23.Puzzles.Day09
+{
+  internal class History
+  {
+    int[] Values { get; }
+
+    public History(string inputLine)
+    {
+      Values = inputLine.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse).ToArray();
+    }
+
+    public int PredictNextValue()
+    {
+      List<int> extrapolated = ExtrapolateNext([.. Values]);
+      return extrapolated[^1];
+    }
+
+    public int PredictPreviousValue()
+    {
+      List<int> extrapolated = ExtrapolatePrevious([.. Values]);
+      return extrapolated[0];
+    }
+
+    private List<int> ExtrapolateNext(List<int> list)
+    {
+      if (list.TrueForAll(i => i == 0))
+      {
+        list.Add(0);
+      }
+      else
+      {
+        List<int> derivative = GetDerivative(list);
+        ExtrapolateNext(derivative);
+        list.Add(list[^1] + derivative[^1]);
+      }
+      return list;
+    }
+
+    private List<int> ExtrapolatePrevious(List<int> list)
+    {
+      if (list.TrueForAll(i => i == 0))
+      {
+        list.Insert(0, 0);
+      }
+      else
+      {
+        List<int> derivative = GetDerivative(list);
+        ExtrapolatePrevious(derivative);
+        list.Insert(0, list[0] - derivative[0]);
+      }
+      return list;
+    }
+
+    private List<int> GetDerivative(List<int> list)
+    {
+      List<int> derivative = [];
+      for (int i = 0; i < list.Count - 1; i++)
+      {
+        derivative.Add(list[i + 1] - list[i]);
+      }
+      return derivative;
+    }
+  }
+}

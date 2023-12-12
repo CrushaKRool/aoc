39f2c8ed124d33a002df8f189148b37abbaac4f1
CrushaKRool <CrushaKRool@users.noreply.github.com>
2023-12-12 19:33:50 +0100

    Solved Day 12 Part 1 and failed to bruteforce Part 2.

---
+++ aoc23/Puzzles/Day12/ConditionRecord.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc23.Puzzles.Day12
+{
+  internal class ConditionRecord
+  {
+    public string Condition { get; set; }
+    public int[] DamagedGroups { get; set; }
+
+    public ConditionRecord(string inputLine)
+    {
+      string[] parts = inputLine.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+      Condition = parts[0];
+      DamagedGroups = parts[1].Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse).ToArray();
+    }
+
+    public void Unfold()
+    {
+      Condition = Condition + '?' + Condition + '?' + Condition + '?' + Condition + '?' + Condition;
+
+      List<int> newGroups = [];
+      newGroups.AddRange(DamagedGroups);
+      newGroups.AddRange(DamagedGroups);
+      newGroups.AddRange(DamagedGroups);
+      newGroups.AddRange(DamagedGroups);
+      newGroups.AddRange(DamagedGroups);
+      DamagedGroups = [.. newGroups];
+    }
+
+    public IEnumerable<string> GetPermutationsOfCondition()
+    {
+      return TryPermutateCondition(Condition);
+    }
+
+    private IEnumerable<string> TryPermutateCondition(string condition)
+    {
+      int wildcardIndex = condition.IndexOf('?');
+      if (wildcardIndex == -1)
+      {
+        // If the input contains no more wildcards, we have reached the base case of the recursion.
+        yield return condition;
+      }
+      else
+      {
+        // Otherwise replace the first encountered wildcard with both possible substitutions and recurse.
+        List<string> result = [];
+        StringBuilder sb = new(condition);
+        sb[wildcardIndex] = '.';
+        result.AddRange(TryPermutateCondition(sb.ToString()));
+        sb[wildcardIndex] = '#';
+        result.AddRange(TryPermutateCondition(sb.ToString()));
+        foreach (var r in result)
+        {
+          yield return r;
+        }
+      }
+    }
+
+    public bool ConditionMatchesDamagedGroups(string condition)
+    {
+      List<int> groups = [];
+      bool curIsDamaged = condition[0] == '#';
+      int curDamagedStreak = curIsDamaged ? 1 : 0;
+      for (int i = 1; i < condition.Length; i++)
+      {
+        bool newIsDamaged = condition[i] == '#';
+        if (newIsDamaged)
+        {
+          curDamagedStreak++;
+        }
+        else
+        {
+          if (curIsDamaged && curDamagedStreak > 0)
+          {
+            groups.Add(curDamagedStreak);
+            curDamagedStreak = 0;
+          }
+        }
+        curIsDamaged = newIsDamaged;
+      }
+      if (curDamagedStreak > 0)
+      {
+        groups.Add(curDamagedStreak);
+      }
+      return groups.SequenceEqual(DamagedGroups);
+    }
+  }
+}
--- aoc23/Puzzles/Day12/Day12.cs
+++ aoc23/Puzzles/Day12/Day12.cs
@@ -1,17 +1,54 @@
 ﻿using aoc_common;
+using System.Linq;
 
 namespace aoc23.Puzzles.Day12
 {
   public class Day12 : IPuzzle
   {
-    public string PuzzleName => throw new NotImplementedException();
+    public string PuzzleName => "Day 12: Hot Springs";
 
-    public string InputFileName => @"Test.txt";
+    public string InputFileName => @"Input.txt";
 
     public void Run(string input)
     {
+      Part1(input);
+      Console.WriteLine("-------------------------------------------");
+      Part2(input);
+    }
+
+    private static void Part1(string input)
+    {
       Console.WriteLine("Part 1:");
-      throw new NotImplementedException();
+      List<ConditionRecord> records = [];
+      foreach (var inputLine in input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+      {
+        records.Add(new(inputLine));
+      }
+
+      int sumOfArrangements = records.AsParallel().Sum(GetArrangementCount);
+      Console.WriteLine($"Sum of arrangements: {sumOfArrangements}");
+    }
+
+    private static void Part2(string input)
+    {
+      Console.WriteLine("Part 1:");
+      List<ConditionRecord> records = [];
+      foreach (var inputLine in input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+      {
+        var rec = new ConditionRecord(inputLine);
+        rec.Unfold();
+        records.Add(rec);
+      }
+
+      int sumOfArrangements = records.AsParallel().Sum(GetArrangementCount);
+      Console.WriteLine($"Sum of arrangements: {sumOfArrangements}");
+    }
+
+    private static int GetArrangementCount(ConditionRecord rec)
+    {
+      int arrangements = rec.GetPermutationsOfCondition().Count(perm => rec.ConditionMatchesDamagedGroups(perm));
+      Console.WriteLine($"{rec.Condition} has {arrangements} arrangements");
+      return arrangements;
     }
   }
 }

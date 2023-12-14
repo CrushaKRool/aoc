18239939c14dd8710fafe906f746861155ecba25
CrushaKRool <CrushaKRool@users.noreply.github.com>
2023-12-14 23:36:15 +0100

    Solved Day 14 Part 1.

--- aoc23/Puzzles/Day14/Day14.cs
+++ aoc23/Puzzles/Day14/Day14.cs
@@ -4,14 +4,55 @@ namespace aoc23.Puzzles.Day14
 {
   public class Day14 : IPuzzle
   {
-    public string PuzzleName => throw new NotImplementedException();
+    public string PuzzleName => "Day 14: Parabolic Reflector Dish";
 
-    public string InputFileName => @"Test.txt";
+    public string InputFileName => @"Input.txt";
 
     public void Run(string input)
     {
+      Part1(input);
+      Console.WriteLine("------------------------------------------------");
+      Part2(input);
+    }
+
+    private static void Part1(string input)
+    {
       Console.WriteLine("Part 1:");
-      throw new NotImplementedException();
+      Platform platform = new(input);
+      platform.TiltNorth();
+      platform.Print();
+      long load = platform.MeasureNorthLoad();
+      Console.WriteLine($"North load is {load}.");
+    }
+
+    private static void Part2(string input)
+    {
+      Console.WriteLine("Part 2:");
+      Platform platform = new(input);
+      for (int i = 0; i < 1000000000; i++)
+      {
+        switch (i % 4)
+        {
+          case 0:
+            platform.TiltNorth();
+            break;
+          case 1:
+            platform.TiltWest();
+            break;
+          case 2:
+            platform.TiltSouth();
+            break;
+          case 3:
+            platform.TiltEast();
+            break;
+        }
+        if (i % 1000 == 0)
+        {
+          Console.WriteLine($"{i} of 1000000000 => {i / 1000000000.0 * 100}%");
+        }
+      }
+      long load = platform.MeasureNorthLoad();
+      Console.WriteLine($"North load is {load}.");
     }
   }
 }
---
+++ aoc23/Puzzles/Day14/Platform.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace aoc23.Puzzles.Day14
+{
+  internal class Platform
+  {
+    public char[][] Data { get; }
+    private int XMax { get; }
+    private int YMax { get; }
+
+    public Platform(string input)
+    {
+      string[] lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+      Data = lines.Select(s => s.ToArray()).ToArray();
+      XMax = lines[0].Length;
+      YMax = lines.Length;
+    }
+
+    public void TiltNorth()
+    {
+      bool changed;
+      do
+      {
+        changed = MoveAllNorth();
+      }
+      while (changed);
+    }
+
+    public void TiltWest()
+    {
+      bool changed;
+      do
+      {
+        changed = MoveAllWest();
+      }
+      while (changed);
+    }
+
+    public void TiltSouth()
+    {
+      bool changed;
+      do
+      {
+        changed = MoveAllSouth();
+      }
+      while (changed);
+    }
+
+    public void TiltEast()
+    {
+      bool changed;
+      do
+      {
+        changed = MoveAllEast();
+      }
+      while (changed);
+    }
+
+    /// <summary>
+    /// Moves all loose rocks 1 step north, if possible.
+    /// </summary>
+    /// <returns>True if any rocks moved this iteration.</returns>
+    private bool MoveAllNorth()
+    {
+      bool changed = false;
+      for (int y = 1; y < YMax; y++)
+      {
+        for (int x = 0; x < XMax; x++)
+        {
+          char oldPos = Data[y][x];
+          if (oldPos != 'O')
+          {
+            continue;
+          }
+          char newPos = Data[y - 1][x];
+          if (newPos != '.')
+          {
+            continue;
+          }
+          Data[y - 1][x] = oldPos;
+          Data[y][x] = '.';
+          changed = true;
+        }
+      }
+      return changed;
+    }
+
+    /// <summary>
+    /// Moves all loose rocks 1 step west, if possible.
+    /// </summary>
+    /// <returns>True if any rocks moved this iteration.</returns>
+    private bool MoveAllWest()
+    {
+      bool changed = false;
+      for (int x = 1; x < XMax; x++)
+      {
+        for (int y = 0; y < YMax; y++)
+        {
+          char oldPos = Data[y][x];
+          if (oldPos != 'O')
+          {
+            continue;
+          }
+          char newPos = Data[y][x - 1];
+          if (newPos != '.')
+          {
+            continue;
+          }
+          Data[y][x - 1] = oldPos;
+          Data[y][x] = '.';
+          changed = true;
+        }
+      }
+      return changed;
+    }
+
+    /// <summary>
+    /// Moves all loose rocks 1 step south, if possible.
+    /// </summary>
+    /// <returns>True if any rocks moved this iteration.</returns>
+    private bool MoveAllSouth()
+    {
+      bool changed = false;
+      for (int y = YMax - 2; y >= 0; y--)
+      {
+        for (int x = 0; x < XMax; x++)
+        {
+          char oldPos = Data[y][x];
+          if (oldPos != 'O')
+          {
+            continue;
+          }
+          char newPos = Data[y + 1][x];
+          if (newPos != '.')
+          {
+            continue;
+          }
+          Data[y + 1][x] = oldPos;
+          Data[y][x] = '.';
+          changed = true;
+        }
+      }
+      return changed;
+    }
+
+    /// <summary>
+    /// Moves all loose rocks 1 step east, if possible.
+    /// </summary>
+    /// <returns>True if any rocks moved this iteration.</returns>
+    private bool MoveAllEast()
+    {
+      bool changed = false;
+      for (int x = XMax - 2; x >= 0; x--)
+      {
+        for (int y = 0; y < YMax; y++)
+        {
+          char oldPos = Data[y][x];
+          if (oldPos != 'O')
+          {
+            continue;
+          }
+          char newPos = Data[y][x + 1];
+          if (newPos != '.')
+          {
+            continue;
+          }
+          Data[y][x + 1] = oldPos;
+          Data[y][x] = '.';
+          changed = true;
+        }
+      }
+      return changed;
+    }
+
+    public long MeasureNorthLoad()
+    {
+      long load = 0;
+      for (int y = 0; y < YMax; y++)
+      {
+        for (int x = 0; x < XMax; x++)
+        {
+          char pos = Data[y][x];
+          if (pos == 'O')
+          {
+            load += YMax - y;
+          }
+        }
+      }
+      return load;
+    }
+
+    public void Print()
+    {
+      for (int y = 0; y < YMax; y++)
+      {
+        for (int x = 0; x < XMax; x++)
+        {
+          Console.Write(Data[y][x]);
+        }
+        Console.WriteLine();
+      }
+    }
+  }
+}
